6ef4b350d7fa0fda074f8673002fcdf8c4fc3ed5
Hanami <95147897+Hanamixp@users.noreply.github.com>
2021-11-28 15:03:42 +0300

    Add files via upload

---
+++ aminos/src/headers.py
@@ -0,0 +1,7 @@
+headers = None
+
+
+class Headers:
+    def __init__(self):
+        if headers:
+            self.headers = headers
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from aminos.__init__ import version
+with open("README.md", "r") as readme: long_description = readme.read()
+
+
+setup(
+    name="Amino-Socket",
+    version=version,
+    url="https://github.com/hanamixp/Amino-Socket.py.git",
+    download_url="https://github.com/hanamixp/Amino-Socket/tarball/master",
+    license="MIT",
+    author="Hanami",
+    author_email="hanamixp@gmail.com",
+    description="Python API to Connect Amino wss",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "aminoapps",
+        "amino",
+        "narvii",
+        "api",
+        "hanami",
+        "amino-socket"
+    ],
+    install_requires=[
+        "websocket-client",
+        "requests",
+    ],
+    setup_requires=["wheel"],
+    project_urls={
+        'Source': 'https://github.com/Hanamixp/Amino-Socket',
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages()
+)
